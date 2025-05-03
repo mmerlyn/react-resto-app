8fac31a24adbbec0da934a8e473853e77ecc0be1
Merlyn Mercylona <merlynmercylona@gmail.com>
2025-05-02 17:50:31 -0700

    menuSlice.js fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,16 +2,31 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';
 import menuReducer from './menuSlice';
 
-// Load cart and menu from localStorage
+const defaultMenu = [
+  {
+    id: 1,
+    name: 'Margherita Pizza',
+    price: 8.99,
+    description: 'Classic pizza with fresh mozzarella, tomato sauce, and basil.',
+    image: 'https://images.unsplash.com/photo-1601924579440-b8a0660c40c4?auto=format&fit=crop&w=800&q=80',
+  },
+  {
+    id: 2,
+    name: 'Arrabiata Pasta',
+    price: 7.99,
+    description: 'Spicy penne pasta in a rich tomato and garlic chili sauce.',
+    image: 'https://images.unsplash.com/photo-1589308078056-fc04a5e1879d?auto=format&fit=crop&w=800&q=80',
+  }
+];
+
 const savedCart = localStorage.getItem('cartState');
 const savedMenu = localStorage.getItem('menuState');
 
 const preloadedState = {
   ...(savedCart && { cart: JSON.parse(savedCart) }),
-  ...(savedMenu && { menu: JSON.parse(savedMenu) }),
+  menu: savedMenu ? JSON.parse(savedMenu) : { items: defaultMenu }
 };
 
-// Create store with both slices
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
@@ -20,7 +35,6 @@ export const store = configureStore({
   preloadedState,
 });
 
-// Save cart and menu to localStorage on every update
 store.subscribe(() => {
   const state = store.getState();
   localStorage.setItem('cartState', JSON.stringify(state.cart));
