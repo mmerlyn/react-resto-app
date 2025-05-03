66c94734412795925d6b739bf38f1dc798b8953f
Merlyn Mercylona <merlynmercylona@gmail.com>
2025-05-02 17:51:37 -0700

    store.js, menuSlice.js fix

--- src/redux/menuSlice.js
+++ src/redux/menuSlice.js
@@ -1,26 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const defaultMenu = [
-  {
-    id: 1,
-    name: 'Margherita Pizza',
-    price: 8.99,
-    description: 'Classic pizza with fresh mozzarella, tomato sauce, and basil.',
-    image: "images/pizza.jpg"
-  },
-  {
-    id: 2,
-    name: 'Arrabiata Pasta',
-    price: 7.99,
-    description: 'Spicy penne pasta in a rich tomato and garlic chili sauce.',
-    image: "images/pasta.jpg",
-  }
-];
-
 const menuSlice = createSlice({
   name: 'menu',
   initialState: {
-    items: defaultMenu
+    items: []
   },
   reducers: {
     setMenu(state, action) {
@@ -29,17 +12,23 @@ const menuSlice = createSlice({
     addMenuItem(state, action) {
       state.items.push(action.payload);
     },
+    deleteMenuItem(state, action) {
+      state.items = state.items.filter(item => item.id !== action.payload);
+    },
     updateMenuItem(state, action) {
       const index = state.items.findIndex(item => item.id === action.payload.id);
       if (index !== -1) {
         state.items[index] = action.payload;
       }
-    },
-    deleteMenuItem(state, action) {
-      state.items = state.items.filter(item => item.id !== action.payload);
     }
   }
 });
 
-export const { setMenu, addMenuItem, updateMenuItem, deleteMenuItem } = menuSlice.actions;
+export const {
+  setMenu,
+  addMenuItem,
+  deleteMenuItem,
+  updateMenuItem
+} = menuSlice.actions;
+
 export default menuSlice.reducer;
